2696f6621a8d5d62488b4c2a07347e4493498be7
Douglas Tomacheski de Abreu e Silva <dotomasi@outlook.com>
2023-02-06 21:41:20 -0300

    - New Components and Fonts

---
+++ src/app/shared/components/display/ac-header/ac-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ac-header',
+  templateUrl: './ac-header.component.html',
+  styleUrls: ['./ac-header.component.scss']
+})
+
+export class AcHeaderComponent {
+
+}
--- src/app/shared/components/display/display-components.module.ts.module.ts
+++ src/app/shared/components/display/display-components.module.ts.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AcHeaderComponent } from './ac-header/ac-header.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AcHeaderComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    AcHeaderComponent
   ]
 })
 export class DisplayComponentsModuleTsModule { }
