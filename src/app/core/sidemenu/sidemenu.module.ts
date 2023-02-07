cddaf91f6b4c24ebc428abf05e50acb6af68ffb8
Douglas Tomacheski de Abreu e Silva <dotomasi@outlook.com>
2023-02-06 22:28:38 -0300

    - New Sidemenu Styles

--- src/app/core/sidemenu/sidemenu.module.ts
+++ src/app/core/sidemenu/sidemenu.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AcSidemenuComponent } from './ac-sidemenu/ac-sidemenu.component';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AcSidemenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    NgbTooltipModule,
+    FontAwesomeModule
   ],
   exports: [
     AcSidemenuComponent
